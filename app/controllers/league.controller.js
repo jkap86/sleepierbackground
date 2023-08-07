369a30bea351b51c8b56aa295ace875babc34d6a
Jay Kapoor <jkap86@gmail.com>
2023-08-07 08:39:18 -0400

    -

--- app/controllers/league.controller.js
+++ app/controllers/league.controller.js
@@ -23,7 +23,7 @@ exports.league = async (app) => {
 
     if (league_ids_to_add.length < total_batch_size) {
         let leagues_db = await League.findAll({
-            order: [['league_id', 'ASC']],
+            order: [['createdAt', 'ASC']],
             limit: total_batch_size - league_ids_to_add.length,
             attributes: ['league_id'],
             raw: true
